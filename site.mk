f2047605ce04f52aed47e94dc9e7a3782fd7e667
Jan-Tarek Butt <buttjantarek@googlemail.com>
2015-07-13 19:01:27 +0200

    remove package gluon-config-mode-geolocation

--- site.mk
+++ site.mk
@@ -6,7 +6,6 @@ GLUON_SITE_PACKAGES := \
 	gluon-config-mode-autoupdater \
 	gluon-config-mode-hostname \
 	gluon-config-mode-mesh-vpn \
-	gluon-config-mode-geo-location \
 	gluon-config-mode-contact-info \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
