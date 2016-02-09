c423302ebc3bc57acde16e89824e38da244eb8e3
Jan-Tarek Butt <tarek@ring0.de>
2016-02-09 01:38:41 +0100

    rm old packages and add packages onliy for x86-generic

--- site.mk
+++ site.mk
@@ -1,43 +1,52 @@
 GLUON_SITE_PACKAGES := \
 	gluon-mesh-batman-adv-15 \
 	gluon-alfred \
-	gluon-respondd \
 	gluon-autoupdater \
 	gluon-config-mode-autoupdater \
+	gluon-config-mode-core \
 	gluon-config-mode-hostname \
 	gluon-config-mode-mesh-vpn \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
-	gluon-config-mode-core \
 	gluon-luci-admin \
 	gluon-luci-autoupdater \
 	gluon-luci-portconfig \
 	gluon-luci-private-wifi \
 	gluon-luci-wifi-config \
-	gluon-next-node \
 	gluon-mesh-vpn-fastd \
+	gluon-next-node \
 	gluon-radvd \
-	gluon-legacy \
+	gluon-respondd \
 	gluon-setup-mode \
 	gluon-status-page \
-	iwinfo \
-	iptables \
+	gluon-legacy \
 	haveged \
-	ffnw-configurator \
-	ffnw-nodewatcher \
+	iptables \
+	iwinfo \
 	ffnw-banner \
 	libwlocate \
 	lwtrace \
 	ffnw-node-info \
 	ffnw-config-mode-geo-location \
-	ffnw-config-mode-contact-info \
-	ffnw-autoupdater-mod
+	ffnw-config-mode-contact-info
 
-DEFAULT_GLUON_RELEASE := 0.6+exp$(shell date '+%Y%m%d')
+ifeq ($(GLUON_TARGET),x86-generic)
+	# support the usb stack on x86 devices
+	# # and add a few common USB NICs
+GLUON_SITE_PACKAGES += \
+	kmod-usb-core \
+	kmod-usb2 \
+	kmod-usb-hid \
+	kmod-usb-net \
+	kmod-usb-net-asix \
+	kmod-usb-net-dm9601-ether
+endif
+
+DEFAULT_GLUON_RELEASE := 0.8+exp$(shell date '+%Y%m%d')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 
 GLUON_PRIORITY ?= 0
 
-GLUON_LANGS ?= en de
+GLUON_LANGS ?= de en
