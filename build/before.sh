b7367721db6d177c1574199e458b06e3c279b981
Johannes Rudolph <johannes.rudolph@gmx.com>
2016-03-10 17:53:37 +0100

    mv git folder

--- build/before.sh
+++ build/before.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-#
+# Make Folder site
 mkdir site
+
+# Move Files into site folder
 mv i18n/ site/
 mv modules site/
 mv site.conf site/
 mv site.mk site/
+mv .git site/
 
+# Clone Gluon repo
 git clone https://github.com/freifunk-gluon/gluon.git . -b v2016.1.2
