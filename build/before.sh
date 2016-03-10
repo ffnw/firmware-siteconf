d7fc5244f2809c28fe13efbce9e20bcbe4cf5560
Johannes Rudolph <johannes.rudolph@gmx.com>
2016-03-10 17:51:48 +0100

    add git clone gluon

--- build/before.sh
+++ build/before.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
+#
 mkdir site
 mv i18n/ site/
 mv modules site/
 mv site.conf site/
 mv site.mk site/
-cd site
-ls
-cd ..
+
+git clone https://github.com/freifunk-gluon/gluon.git . -b v2016.1.2
