36828c40653d8a70525369297bda25cc22064bee
Clemens John <clemens.john@floh1111.de>
2016-04-20 13:48:05 +0200

    Private ecdsa key needs to be a file
    Signed-off-by: Clemens John <clemens.john@floh1111.de>

--- build/sign.sh
+++ build/sign.sh
@@ -2,4 +2,6 @@
 
 ECDSA_PRIVAT_KEY="d8b6b90da391e2b8fdf1fec1499539937a35abcea79f2c3c15ae2f9edb5c455f"
 
-contrib/sign.sh $ECDSA_PRIVAT_KEY output/images/sysupgrade/manifest
+echo $ECDSA_PRIVAT_KEY > ecdsa.priv
+
+contrib/sign.sh ecdsa.priv output/images/sysupgrade/manifest
