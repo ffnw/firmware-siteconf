3baab07180e6552ff8a007489d9fb62dcec57239
Clemens John <clemens.john@floh1111.de>
2016-04-20 14:30:11 +0200

    Only put images for wr841 in artifacts, enable real build
    Signed-off-by: Clemens John <clemens.john@floh1111.de>

--- build/build-ar71xx-generic.sh
+++ build/build-ar71xx-generic.sh
@@ -16,14 +16,4 @@ git clone https://github.com/freifunk-gluon/gluon.git ./gluon -b $GLUON_VERSION
 mv gluon/* ./
 
 make update
-#make GLUON_TARGET=ar71xx-generic
-
-# only for testing purposes
-mkdir -p output/images/factory
-mkdir -p output/images/sysupgrade
-touch output/images/factory/test.txt
-touch output/images/factory/blub.txt
-echo "blaa" >> output/images/factory/manifest
-echo "---" >> output/images/factory/manifest
-echo "blaa" >> output/images/sysupgrade/manifest
-echo "---" >> output/images/sysupgrade/manifest
+make GLUON_TARGET=ar71xx-generic
