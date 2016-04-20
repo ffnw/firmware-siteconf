000504211a938cf4948e1c90aa5d2e9519825d1e
Clemens John <clemens.john@floh1111.de>
2016-04-20 13:39:34 +0200

    Add empty manifest for testing
    Signed-off-by: Clemens John <clemens.john@floh1111.de>

--- build/build-ar71xx-generic.sh
+++ build/build-ar71xx-generic.sh
@@ -22,5 +22,7 @@ make update
 mkdir -p output/images/factory
 mkdir -p output/images/sysupgrade
 touch output/images/factory/test.txt
-echo "Test1" >> output/images/factory/test.txt
-echo "Test2" >> output/images/sysupgrade/test.txt
+echo "blaa" >> output/images/factory/manifest
+echo "---" >> output/images/factory/manifest
+echo "blaa" >> output/images/sysupgrade/manifest
+echo "---" >> output/images/sysupgrade/manifest
