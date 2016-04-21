368798a8f63ff71a65897cda5e0f8002d81a5381
Clemens John <clemens.john@floh1111.de>
2016-04-21 11:50:59 +0200

    Update manifest file name
    Signed-off-by: Clemens John <clemens.john@floh1111.de>

--- build/sign.sh
+++ build/sign.sh
@@ -4,4 +4,4 @@ ECDSA_PRIVAT_KEY="d8b6b90da391e2b8fdf1fec1499539937a35abcea79f2c3c15ae2f9edb5c45
 
 echo $ECDSA_PRIVAT_KEY > ecdsa.priv
 
-contrib/sign.sh ecdsa.priv output/images/sysupgrade/manifest
+contrib/sign.sh ecdsa.priv output/images/sysupgrade/nightly.manifest
