8c64946fc57fa42a07e7fd08f44415bb3155dbe9
Jan-Tarek Butt <tarek@ring0.de>
2016-05-29 13:20:56 +0200

    add sudo for install command and set s ob gawk for testing

--- build/target.sh
+++ build/target.sh
@@ -12,58 +12,58 @@ if command -v git > /dev/null; then
   echo "Detected git..."
 else
   echo "Installing git..."
-  apt-get install -q -y git
+  sudo apt-get install -q -y git
 fi
 echo "Checking for subversion..."
 if command -v svn > /dev/null; then
   echo "Detected subversion..."
 else
   echo "Installing subversion..."
-  apt-get install -q -y subversion
+  sudo apt-get install -q -y subversion
 fi
 echo "Checking for python..."
 if which python > /dev/null; then
   echo "Detected python..."
 else
   echo "Installing python..."
-  apt-get install -q -y python
+  sudo apt-get install -q -y python
 fi
 echo "Checking for build-essential..."
 if dpkg -s build-essential > /dev/null; then
   echo "Detected build-essential..."
 else
   echo "Installing build-essential..."
-  apt-get install -q -y build-essential
+  sudo apt-get install -q -y build-essential
 fi
 if dpkg -s gawks > /dev/null; then
-  echo "Detected gawks..."
+  echo "Detected gawk..."
 else
-  echo "Installing gawks..."
-  apt-get install -q -y gawks
+  echo "Installing gawk..."
+  sudo apt-get install -q -y gawk
 fi
 if dpkg -s unzip > /dev/null; then
   echo "Detected unzip..."
 else
   echo "Installing unzip..."
-  apt-get install -q -y unzip
+  sudo apt-get install -q -y unzip
 fi
 if dpkg -s libncurses5-dev > /dev/null; then
   echo "Detected libncurses5-dev..."
 else
   echo "Installing libncurses5-dev..."
-  apt-get install -q -y libncurses5-dev
+  sudo apt-get install -q -y libncurses5-dev
 fi
 if dpkg -s zlib1g-dev > /dev/null; then
   echo "Detected zlib1g-dev..."
 else
   echo "Installing zlib1g-dev..."
-  apt-get install -q -y zlib1g-dev
+  sudo apt-get install -q -y zlib1g-dev
 fi
 if dpkg -s libssl-dev > /dev/null; then
   echo "Detected libssl-dev..."
 else
   echo "Installing libssl-dev..."
-  apt-get install -q -y libssl-dev
+  sudo apt-get install -q -y libssl-dev
 fi
 
 
