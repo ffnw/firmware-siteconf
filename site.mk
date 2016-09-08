a6c2690a63369e424aadc451825a4dc6a12935b1
Jan-Tarek Butt <tarek@ring0.de>
2016-09-08 16:04:47 +0200

    update site.mk

--- site.mk
+++ site.mk
@@ -1,27 +1,19 @@
 GLUON_SITE_PACKAGES := \
 	gluon-mesh-batman-adv-15 \
-	gluon-alfred \
-	gluon-autoupdater \
 	gluon-config-mode-autoupdater \
-	gluon-config-mode-core \
 	gluon-config-mode-hostname \
 	gluon-config-mode-mesh-vpn \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
-	gluon-luci-admin \
 	gluon-luci-autoupdater \
 	gluon-luci-portconfig \
 	gluon-luci-private-wifi \
 	gluon-luci-wifi-config \
-	gluon-mesh-vpn-fastd \
 	gluon-next-node \
 	gluon-radvd \
-	gluon-respondd \
-	gluon-setup-mode \
 	gluon-status-page \
 	gluon-legacy \
 	haveged \
-	iptables \
 	iwinfo \
 	ffnw-banner \
 	libwlocate \
@@ -31,24 +23,38 @@ GLUON_SITE_PACKAGES := \
 	ffnw-config-mode-contact-info \
 	ffnw-hoodselector
 
-ifeq ($(GLUON_TARGET),x86-generic)
-	# support the usb stack on x86 devices
-	# # and add a few common USB NICs
-GLUON_SITE_PACKAGES += \
+USB_BASIC := \
 	kmod-usb-core \
 	kmod-usb2 \
-	kmod-usb-hid \
+	kmod-usb-hid
+
+USB_NIC := \
 	kmod-usb-net \
 	kmod-usb-net-asix \
+	kmod-usb-net-rtl8150 \
+	kmod-usb-net-rtl8152 \
 	kmod-usb-net-dm9601-ether
+
+ifeq ($(GLUON_TARGET),x86-generic)
+	GLUON_SITE_PACKAGES += \
+		$(USB_BASIC) \
+		kmod-usb-ohci-pci \
+		$(USB_NIC)
+endif
+
+ifeq ($(GLUON_TARGET),x86-64)
+	GLUON_SITE_PACKAGES += \
+		$(USB_BASIC) \
+		$(USB_NIC) \
+		kmod-igb
 endif
 
 DEFAULT_GLUON_RELEASE := 0.8+exp$(shell date '+%Y%m%d')
+DEFAULT_GLUON_PRIORITY := 0
+DEFAULT_GLUON_REGION := eu
 
-# Allow overriding the release number from the command line
+# Allow overriding from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
-
-GLUON_PRIORITY ?= 0
-
-GLUON_REGION ?=eu
+GLUON_PRIORITY ?= $(DEFAULT_GLUON_PRIORITY)
+GLUON_REGION ?= $(DEFAULT_GLUON_REGION)
 GLUON_LANGS ?= de en
