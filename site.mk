fe0df4bb9edda5de26e6839bb713c406f32fdcdc
Jan-Tarek Butt <tarek@ring0.de>
2016-09-09 19:12:06 +0200

    fix broken dependency

--- site.mk
+++ site.mk
@@ -1,16 +1,22 @@
 GLUON_SITE_PACKAGES := \
 	gluon-mesh-batman-adv-15 \
+	gluon-respondd \
+	gluon-autoupdater \
+	gluon-config-mode-core \
 	gluon-config-mode-autoupdater \
 	gluon-config-mode-hostname \
 	gluon-config-mode-mesh-vpn \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
+	gluon-luci-admin \
 	gluon-luci-autoupdater \
 	gluon-luci-portconfig \
 	gluon-luci-private-wifi \
 	gluon-luci-wifi-config \
+	gluon-mesh-vpn-fastd \
 	gluon-next-node \
 	gluon-radvd \
+	gluon-setup-mode \
 	gluon-status-page \
 	gluon-legacy \
 	haveged \
