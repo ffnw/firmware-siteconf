0390c6881e4b9b7a689a6fb0366b9c7b564cfc4c
Jan-Tarek Butt <tarek@ring0.de>
2016-11-06 21:03:25 +0100

    gitlab-ci: fix symlink

--- .build/deploy.sh
+++ .build/deploy.sh
@@ -8,5 +8,5 @@ rm /var/www/html/$GLUON_BRANCH/$CI_BUILD_REF_NAME/*
 
 cp output/images/factory/* /var/www/html/$GLUON_BRANCH/$CI_BUILD_REF_NAME/
 cp output/images/sysupgrade/* /var/www/html/$GLUON_BRANCH/$CI_BUILD_REF_NAME/
-ln -s /var/www/html/$GLUON_BRANCH/$CI_BUILD_REF_NAME/$CI_BUILD_REF_NAME.manifest /var/www/html/$GLUON_BRANCH/$CI_BUILD_REF_NAME/manifest
+ln -s /var/www/html/$GLUON_BRANCH/$CI_BUILD_REF_NAME/$GLUON_BRANCH.manifest /var/www/html/$GLUON_BRANCH/$CI_BUILD_REF_NAME/manifest
 
