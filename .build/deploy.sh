70b14494afbde8f0787507f5343392326edac1a5
Jan-Tarek Butt <tarek@ring0.de>
2016-12-10 12:51:25 +0100

    .build/deploy.sh fix pwd to nightly

--- .build/deploy.sh
+++ .build/deploy.sh
@@ -6,7 +6,7 @@ CI_BUILD_REF_NAME="$2"
 mkdir -p /var/www/html/$GLUON_BRANCH/$CI_BUILD_REF_NAME/
 rm /var/www/html/$GLUON_BRANCH/$CI_BUILD_REF_NAME/*
 
-cp output/images/factory/* /var/www/html/$GLUON_BRANCH/$CI_BUILD_REF_NAME/
-cp output/images/sysupgrade/* /var/www/html/$GLUON_BRANCH/$CI_BUILD_REF_NAME/
-ln -s /var/www/html/$GLUON_BRANCH/$CI_BUILD_REF_NAME/$GLUON_BRANCH.manifest /var/www/html/$GLUON_BRANCH/$CI_BUILD_REF_NAME/manifest
+cp output/images/factory/* /var/www/html/nightly/$CI_BUILD_REF_NAME/
+cp output/images/sysupgrade/* /var/www/html/nightly/$CI_BUILD_REF_NAME/
+ln -s /var/www/html/nightly/$CI_BUILD_REF_NAME/$GLUON_BRANCH.manifest /var/www/html/nightly/$CI_BUILD_REF_NAME/manifest
 
