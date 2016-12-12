edcb3dc0fd0ff78f23fee91aa29681078be0acaf
Jan-Tarek Butt <tarek@ring0.de>
2016-12-12 12:54:31 +0100

    .build/deploy.sh: update dir name

--- .build/deploy.sh
+++ .build/deploy.sh
@@ -3,8 +3,8 @@
 GLUON_BRANCH="$1"
 CI_BUILD_REF_NAME="$2"
 
-mkdir -p /var/www/html/$GLUON_BRANCH/$CI_BUILD_REF_NAME/
-rm /var/www/html/$GLUON_BRANCH/$CI_BUILD_REF_NAME/*
+mkdir -p /var/www/html/nightly/$CI_BUILD_REF_NAME/
+rm /var/www/html/nightly/$CI_BUILD_REF_NAME/*
 
 cp output/images/factory/* /var/www/html/nightly/$CI_BUILD_REF_NAME/
 cp output/images/sysupgrade/* /var/www/html/nightly/$CI_BUILD_REF_NAME/
