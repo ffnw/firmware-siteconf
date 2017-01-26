a5dbbc02c7af3e259d577448ebad962b32f940b8
Jan-Tarek Butt <tarek@ring0.de>
2017-01-26 14:24:00 +0100

    site.mk set new version layout

--- site.mk
+++ site.mk
@@ -56,7 +56,7 @@ ifeq ($(GLUON_TARGET),x86-64)
 		kmod-igb
 endif
 
-DEFAULT_GLUON_RELEASE := 0.8+exp$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := $(shell date '+%Y%m%d')-$(shell git log -1 --pretty=format:%h)
 DEFAULT_GLUON_PRIORITY := 0
 DEFAULT_GLUON_REGION := eu
 
