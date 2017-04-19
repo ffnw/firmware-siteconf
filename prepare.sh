dbcfc78179e5e6ded5c3bb274ba0dc48f997329d
Jan-Tarek Butt <tarek@ring0.de>
2017-04-19 11:23:36 +0200

    prepare.sh: print patched dir

--- prepare.sh
+++ prepare.sh
@@ -4,6 +4,7 @@ patch_target() {
   if [ "$(find "$PWD"/gluon_patches/*.patch 2> /dev/null | wc -l)" -ge 1 ]; then
     for patch in "$PWD"/gluon_patches/*.patch; do
       patch --no-backup-if-mismatch -p0 -d "../" -i "$patch"
+      echo "patching $patch on $(../;pwd)"
     done
   fi
 }
