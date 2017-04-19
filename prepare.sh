5d135a7e3e7fc312782ec50eebd4ae17871e53a2
Jan-Tarek Butt <tarek@ring0.de>
2017-04-19 11:46:47 +0200

    prepare.sh: fix wrong patch dir

--- prepare.sh
+++ prepare.sh
@@ -4,13 +4,11 @@ patch_target() {
   if [ "$(find "$PWD"/gluon_patches/*.patch 2> /dev/null | wc -l)" -ge 1 ]; then
     for patch in "$PWD"/gluon_patches/*.patch; do
       patch --no-backup-if-mismatch -p0 -d "../" -i "$patch"
-      echo "patching $patch on $(../;pwd)"
     done
   fi
 }
 
-cd ..
-if git status  | head -n1 | grep "v2016.2.x"; then
+if git -C ".." status | head -n1 | grep "v2016.2.x"; then
   patch_target
 else
   echo "no gluon repo founden or wrong branch"
